3b3c7ef804a01aae88a30fa57e2c42255bc4c655
Evan Muller <theevan117@gmail.com>
2011-11-06 10:35:34 -0500

    Added initial function for choosing other actions to take

--- Dice.py
+++ Dice.py
@@ -1,22 +1,32 @@
-
+######################################
 import random
 
 from random import randint
+######################################
+
 
 # repeating loop
 while True:
 
+	# status of what user wants to do
+	status = raw_input('Do you want to roll the dice? ')
+	
+	# status condition to leave loop and do something other than roll dice
+	if status == 'no':
+		break
+	else:
+		print 'Okay!'
+	
+	# declaration of dice var
 	dice = int(input('What dice do you want to roll? '))
 	
-	# break conditions
-	if dice < 0:
+	# invalid dice choice
+	if dice <= 0:
 		print 'Dice value not valid, exiting....'
 		break
 		
-	
 	# dice rolling protocol
 	print 'You chose d' + str(dice)
-	
 	print randint(1, dice)
 	
 	
