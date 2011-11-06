bf094d3c8be2964163371e5901e66525d49e2d36
Evan Muller <theevan117@gmail.com>
2011-11-06 01:34:44 -0500

    Code outputs number of die sides in addition to random values

--- Dice.py
+++ Dice.py
@@ -26,25 +26,11 @@ import random
 
 from random import randint
 
-dice = float(input('What dice do you want to roll?'))
+dice = int(input('What dice do you want to roll?'))
 
-# d6 protocol
-if dice == 6:
-	print "You chose d6"
-	print randint(1,6)
-	
-# d8 protocol
-if dice == 8:
-	print "You chose d8"
-	print lambda :randint(1,8)
+# dice rolling protocol
 
-# d12 protocol
-if dice == 12:
-	print "You chose d12"
-	print lambda :randint(1,12)
+print 'You chose d' + str(dice)
 	
-# d20 protocol
-if dice == 20:
-	print "You chose d20"
-	print lambda :randint(1,20)
+print randint(1, dice)
 
