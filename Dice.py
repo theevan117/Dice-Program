6181e7615ae356a2ce097d7a6df4b88a930e8d7a
Evan Muller <theevan117@gmail.com>
2011-11-06 01:13:59 -0500

    Old Archaic Shitty Code

--- Dice.py
+++ Dice.py
@@ -22,9 +22,29 @@
 #       
 #       
 
+import random
+
+from random import randint
+
 dice = float(input('What dice do you want to roll?'))
+
+# d6 protocol
 if dice == 6:
-	print ("You chose d6")
+	print "You chose d6"
+	print randint(1,6)
+	
+# d8 protocol
+if dice == 8:
+	print "You chose d8"
+	print lambda :randint(1,8)
+
+# d12 protocol
+if dice == 12:
+	print "You chose d12"
+	print lambda :randint(1,12)
+	
+# d20 protocol
 if dice == 20:
-	print ("You chose d20")
+	print "You chose d20"
+	print lambda :randint(1,20)
 
