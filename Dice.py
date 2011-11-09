9269a3ad709c62cc7b91f22bf2469f67f4f4112f
Evan Muller <evan@Duncan.(none)>
2011-11-09 17:47:54 -0500

    Implemented dice_history for viewing of previous dice rolls

--- Dice.py
+++ Dice.py
@@ -4,6 +4,7 @@ import random
 from random import randint
 ######################################
 
+dice_history = []
 
 # dice protocol loop
 while True:
@@ -18,20 +19,24 @@ while True:
 	else:
 		break
 		
-	# declaration of dice var
+	# declaration of dice and dice_value vars
 	dice = int(input('What dice do you want to roll? '))
+	dice_value = randint(1, dice)
 	
 	# invalid dice choice
 	if dice <= 0:
 		print 'Dice value not valid, exiting....'
 		break
 		
-	# dice rolling protocol
+	# dice rolling and value storage protocol
 	print 'You chose d' + str(dice)
-	print randint(1, dice)
+	print dice_value
+	dice_history.append(dice_value)
 	
-# declaration of what_do for later actions to be taken	
+# declaration of what_do var	
 what_do = raw_input('Would you like to check your dice rolling history? y/n ')
-
-
+	
+# display of dice_history var
+if what_do == "y" or what_do == "Y":
+	print dice_history
 
